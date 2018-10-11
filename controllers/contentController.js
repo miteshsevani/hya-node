e2fb20f48749b950332a3164a5d4180ed0f728fb
miteshsevani <msevani@gmail.com>
2018-10-11 17:37:16 +0100

    Added content and about page

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -8,23 +8,6 @@ exports.about = function(req, res) {
   res.render('about',{data:data.content.about});
 };
 
-exports.objectives = function(req, res) {
-  res.render('about/objectives',{data:data.content.about.objectives});
-};
-
-exports.history = function(req, res) {
-  res.render('about/objectives',{data:data.content.about.history});
-};
-
-exports.committee = function(req, res) {
-  res.render('about/volunteers',{data:data.content.about.committee});
-};
-
-exports.volunteers = function(req, res) {
-  res.render('about/volunteers',{data:data.content.about.volunteers});
-};
-
 exports.keydates = function(req, res) {
   res.render('keydates',{data:data.content.keydates});
 };
-
--- routes/about.js
+++ routes/about.js
@@ -5,10 +5,6 @@ var contentController = require('../controllers/contentController');
 
 /* GET about page */
 router.get('/', contentController.about);
-router.get('/objectives', contentController.objectives);
-router.get('/history', contentController.history);
-router.get('/volunteers', contentController.volunteers);
-router.get('/committee', contentController.committee);
 
 
 module.exports = router;
